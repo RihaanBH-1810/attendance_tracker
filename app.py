3d4629b4c04895fc0953160a4cbd7b77a87096e2
Rihaan B. H <rihaan1810@gmail.com>
2024-06-08 08:00:05 +0000

    add: user authentication

--- app.py
+++ app.py
@@ -1,36 +1,57 @@
-from flask import Flask, render_template, jsonify, request
-from models import Session, User
+from flask import Flask, render_template, jsonify, request, redirect
+from model import Session, User
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 
 app = Flask(__name__)
 
-@app.route("/login")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        session = Session()
+        data = request.form
+        user = session.query(User).filter_by(user_name=data.get('username')).first()
+        if user:
+            passwordReader = PasswordHasher()
+            try:
+                passwordReader.verify(user.password, data.get('password'))
+                return redirect('/home')
+            except VerifyMismatchError:
+                return render_template('login.html', error='Invalid credentials')
+        else:
+            return render_template('login.html', error='Invalid credentials')
+    else:
+        return render_template('login.html')
 
-@app.route("/add_user", methods = ['POST'])
-def add_user():
-    ession = Session()
-    data = request.json
-    user = User(
-        user_name=data.get('user_name'),
-        password=data.get('password'),
-        daily_labtime=data.get('daily_lab_time', 0),  
-        labtime_data=data.get('uptime_data', {}) 
-    )
-    session.add(user)
-    session.commit()
-    return jsonify({'message': 'User added successfully'}), 201
-
-
-@app.route("/register")
+@app.route("/register", methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        session = Session()
+        data = request.form
+        password_writer = PasswordHasher()
+        hashed_password = password_writer.hash(data.get('password'))
+        user = User(
+            user_name=data.get('username'),
+            password=hashed_password,
+        )
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    else:
+        return render_template('register.html')
 
 @app.route("/mark_attendance", methods = ["POST"])
-def mark_attendance:
-    pass 
+def mark_attendance():
+    pass
 
 @app.route("/dashboard")
-def display_dashboard:
-    pass 
-
+def display_dashboard():
+    pass
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 load_dotenv()
 
 curr_env = os.environ['MODE'] if 'MODE' in os.environ else 'development'
-DB_URL = "" if curr_env == 'production' else "localhost"
+DB_URL = "" if curr_env == 'production' else "127.0.0.1"
 DB_USERNAME = os.getenv('DB_USERNAME') if curr_env == 'production' else "my_user"
 DB_PASSWORD = os.getenv('DB_PASSWORD') if curr_env == 'production' else "my_password"
 DB_NAME = os.getenv('DB_NAME') if curr_env == 'production' else "my_database"
