fc904d2364dc967f668bc2c4441496b1ed19afc7
RihaanBH-1810 <rihaan1810@gmail.com>
2024-06-09 10:08:26 +0530

    add: three more tables attendance,log and log modules

--- model.py
+++ model.py
@@ -15,4 +15,30 @@ class User(Base):
     labtime_data = Column(JSON, nullable=False, default=dict)
     name = Column(Text, nullable=True)
     rollNo = Column(Text, nullable=True)
-    mac = Column(Text, nullable=True)
+    shared_secret = Column(Text, nullable=True)  # Add this field
+
+class Module(Base):
+    __tablename__ = 'modules'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text, nullable=False)
+    SSID = Column(Text, nullable=False, default="amFOSS_")
+    seed = Column(Integer, nullable=False, default=1000)
+    seedRefreshInterval = Column(Interval, nullable=False)
+    lastRefreshTime = Column(DateTime, nullable=False)
+    isPaused = Column(Boolean, nullable=False, default=True)
+
+class Log(Base):
+    __tablename__ = 'logs'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    member_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    member = relationship('User', back_populates='logs')
+    date = Column(DateTime, nullable=False)
+    lastSeen = Column(DateTime, nullable=True)
+    duration = Column(Interval, nullable=True)
+    sessions = Column(JSON, nullable=True)
+    modules = relationship('Module', secondary='log_modules', back_populates='logs')
+
+class LogModules(Base):
+    __tablename__ = 'log_modules'
+    log_id = Column(Integer, ForeignKey('logs.id'), primary_key=True)
+    module_id = Column(Integer, ForeignKey('modules.id'), primary_key=True)
